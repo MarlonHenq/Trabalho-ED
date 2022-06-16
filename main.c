3d21aa36671ed278df5eed47068ec9d05332b41d
MarlonHenq <marlon@marlonhenq.dev>
2022-06-16 16:18:19 -0300

    add structs

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Provável adição de um randon math
 
 //Cores especiais
 #define errorColor     "\x1b[31m"
@@ -8,6 +9,29 @@
 #define contrastColor    "\x1b[36m"
 
 #define resetColor   "\x1b[0m"
+
+//Valores HardCoded 
+const productionTimeVariation = 10; // 10%
+const KWHCust = 1; // 1 real o Kilo Watt Hora
+const hourInSeconds = 3600; //Total de segundos que tem em uma hora
+
+typedef struct produto{
+    int id; //ID que receberá em ordem de carregamento do arquivo
+    char productionType[2]; //NOME abreviado (C - coxinha | P - Peixe | A - Almôndega)
+    float productionCust; //Custo de Produção (EM REAIS)
+    float salePrice; //Preço Atacado (Preço da venda no atacado) (EM REAIS)
+    int deteriorationTime; //Tempo de deteriorização (EM SEGUNDOS) 
+    int productionProbability; //Probabilidade de ser produzido (%)
+}Tproduto;
+
+typedef struct maquina{
+    int id; //ID que receberá em ordem de carregamento do arquivo
+    char model[30]; //Modelo
+    char productionType[2]; //Tipo de produto processado (T repesentando todos)
+    int productionTime; //Tempo de produção quem pode variar +/- 10% (Constante productionTimeVariation)
+    int consumption; //Consumo em KWH
+    int price; //Preço em reais da compra da maquina
+}TMaquina;
 
 void helpMenu();
 
