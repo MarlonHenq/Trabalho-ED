ab528c31c3e354a394830b69ac806c25e7743d6e
MarlonHenq <marlon@marlonhenq.dev>
2022-06-15 23:07:15 -0300

    instruções e cores especiais

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Cores especiais
+#define errorColor     "\x1b[31m"
+#define listColor    "\x1b[34m"
+#define alertColor  "\x1b[33m"
+#define contrastColor    "\x1b[36m"
+
+#define resetColor   "\x1b[0m"
+
 int main(){
     
 
